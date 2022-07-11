6cdabe2796c165d7620116d7a577c58a49b9476c
Hyde Liao <kcritkernel@gmail.com>
2022-07-11 23:59:34 +0800

    [102] Binary Tree Level Order Traversal
    Add BFS solutiton

---
+++ java/102.binary-tree-level-order-traversal.java
@@ -0,0 +1,113 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=102 lang=java
+ *
+ * [102] Binary Tree Level Order Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (61.05%)
+ * Likes:    8968
+ * Dislikes: 173
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given the root of a binary tree, return the level order traversal of its
+ * nodes' values. (i.e., from left to right, level by level).
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [1]
+ * Output: [[1]]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution
+{
+    public List<List<Integer>>
+        levelOrder(TreeNode root)
+    {
+        return this.byBFS(root);
+    }
+
+    private List<List<Integer>>
+        byBFS(TreeNode root)
+    {
+        List<List<Integer>> rtn = new ArrayList<>();
+        if(root == null) {
+            return rtn;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            int cnt = queue.size();
+            for(int ix = 0; ix < cnt; ix++) {
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left != null) {
+                    queue.add(node.left);
+                }
+                if(node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            rtn.add(temp);
+        }
+
+        return rtn;
+    }
+}
+// @lc code=end
+
